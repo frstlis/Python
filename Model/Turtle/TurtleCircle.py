c0c1942744acbc289193385d0b31fc61ba5966cc
frstlis <48125063@qq.com>
2019-12-17 14:32:34 +0800

    Modify README

---
+++ Model/Turtle/TurtleCircle.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.begin_poly()
+turtle.color('read')
+turtle.pensize(3)
+turtle.circle(39)
+turtle.penup()
+
+turtle.goto(0,-200)
+turtle.pendown()
+turtle.circle(80)
+
+turtle.done()
